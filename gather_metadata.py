c8c7fe0a7659356ebcf18685fa0f58cc863e62ee
Robert <rwallhead@gmail.com>
2023-04-24 14:05:17 +1000

    switched to a playlist view, JSON output

--- gather_metadata.py
+++ gather_metadata.py
@@ -1,9 +1,10 @@
+import json
+import re
+import sys
 import time
 
 import requests
 
-
-CHANNEL_ID = 'UCm9K6rby98W8JigLoZOh6FQ'
 
 # To get:
 #   - Create Google Cloud project
@@ -11,43 +12,65 @@ CHANNEL_ID = 'UCm9K6rby98W8JigLoZOh6FQ'
 #   - Create an API key
 API_KEY = ''
 
+PLAYLIST_ID = 'PLpIvUbO_777y3bRSAKeh4Tq_I9TxbbAm4'
+TITLE_RE = re.compile(f'\[(\d+)\]\s+(.*)')
 
-def main(page_size=50):
-    video_ids = set()
-    url = f'https://youtube.googleapis.com/youtube/v3/search'
 
+def extract(item):
+    video_id = item['snippet']['resourceId']['videoId']
+    full_title = item['snippet']['title']
+    match = TITLE_RE.match(full_title)
+    if match is None:
+        return
+
+    number, short_title = match.groups()
+    return {
+        'id': video_id,
+        'number': int(number),
+        'fullTitle': full_title,
+        'shortTitle': short_title,
+        'url': f'https://www.youtube.com/watch?v={video_id}'
+    }
+
+
+def main(page_size=50, sleep=0.5):
+    dataset = {}
+
+    url = 'https://youtube.googleapis.com/youtube/v3/playlistItems'
     page_token = None
     while True:
         payload = requests.get(
-            url,
-            params={
-                'channelId': CHANNEL_ID,
-                'key': API_KEY,
-                'maxResults': page_size,
-                'pageToken': page_token,
-                'type': 'video'
-            },
-            headers={
-                'Accept': 'application/json'
-            }
-        ).json()
+                url,
+                params={
+                    'playlistId': PLAYLIST_ID,
+                    'key': API_KEY,
+                    'maxResults': page_size,
+                    'pageToken': page_token,
+                    'part': 'snippet'
+                },
+                headers={
+                    'Accept': 'application/json'
+                }
+            ).json()
 
-        ids = [item['id']['videoId']
-               for item
-               in payload['items']]
-
-        video_ids.update(ids)
+        for item in payload['items']:
+            data = extract(item)
+            if data is None:
+                continue
+            dataset[data['number']] = data
 
         if payload.get('nextPageToken') is None:
-            print(payload)
             break
 
         page_token = payload['nextPageToken']
 
-        time.sleep(1)
+        time.sleep(sleep)
+        sys.stdout.write('.')
+        sys.stdout.flush()
 
+    with open('dataset.json', 'w') as df:
+        df.write(json.dumps({'dataset': dataset}, sort_keys=True, indent=2))
 
-    print(sorted(list(video_ids)))
 
 if __name__ == '__main__':
     main()
