c9e66bc4c8dfd27e976501c70bbfd9362a02fd47
Robert <rwallhead@gmail.com>
2023-04-25 10:39:15 +1000

    Much better searching

--- docs/script.js
+++ docs/script.js
@@ -7,14 +7,45 @@ const search = (function () { // eslint-disable-line no-unused-vars
     return response.json()
   }
 
+  const normalise = function (query) {
+    return query.split(' ').map(part => {
+      return part.toLowerCase().replace(/[^\w]+$/i, '').replace(/^[^\w]+/i, '')
+    }).filter(part => {
+      return part.length > 1
+    }).join(' ')
+  }
+
   const search = function (query) {
-    let results = []
+    query = normalise(query)
+    const queryTokens = query.split(' ')
+
     if (query.length < 3) {
-      _searchTarget.innerText = results
+      _searchTarget.innerText = 'Enter more'
       return
     }
 
-    results = Object.fromEntries(Object.entries(_dataset).filter(([k, v]) => v.shortTitle.toLowerCase().includes(query.toLowerCase())))
+    const scores = Object.fromEntries(Object.entries(_dataset).map(([key, value]) => [key, 0]))
+    const keys = Object.keys(scores)
+
+    // Simple overlapping search.
+    keys.forEach(key => {
+      if (_dataset[key].search.includes(query)) {
+        scores[key] += 1
+      }
+    })
+
+    // Percentage of full token overlap
+    keys.forEach(key => {
+      queryTokens.forEach(queryToken => {
+        if (_dataset[key].searchTokens.includes(queryToken)) {
+          scores[key] += 1
+        }
+      })
+    })
+
+    const results = Object.entries(_dataset).filter(([key, value]) => {
+      return scores[key] > 0
+    })
 
     _searchTarget.innerText = JSON.stringify(results)
   }
@@ -23,7 +54,11 @@ const search = (function () { // eslint-disable-line no-unused-vars
     _searchTarget = document.querySelector(resultsListSelector)
 
     loadData().then((data) => {
-      _dataset = data.dataset
+      _dataset = Object.fromEntries(Object.entries(data.dataset).map(([key, value]) => {
+        value.search = normalise(value.shortTitle)
+        value.searchTokens = value.search.split(' ')
+        return [key, value]
+      }))
     })
 
     document
